9a8a0b3d33796c0643cc3c1bf71721955bbec311
Jonathan <jonathankendrick697@gmail.com>
2023-08-04 10:11:05 -0700

    changed the UI

--- app.js
+++ app.js
@@ -1,100 +1,112 @@
-//when keys are press display values on screen
-//get the first values inputed and store it
-//get the operator that was clicked and save it
-//get the second value after an operator was clicked and store it
-//when the equal key is pressed calculate the first value and second value and display the answe
-let display = document.getElementById('display');
-let keys = document.querySelectorAll('.key');
+//when keys are pressed the number should be displayed on the calculator screen
+//save the first and second value  in a variable
+//when and operator is pressed right after a number was pressed the number and the operator should be diplay in the previous-oprend div
+//you should be able to do basic operator with the calculator and display the answers in a div
 
-let isOperatorClicked = false;
-let previousAction = display.dataset.previousAction;
+let keys = document.querySelectorAll('.key');
+let previousDisplayValue = document.querySelector('.previous-operand');
+let CurrentDisplayValue = document.querySelector('.current-operand');
 
 keys.forEach((key) => {
-  key.addEventListener('click', function () {
-    keys.forEach((key) => key.classList.remove('active'));
+  CurrentDisplayValue.textContent = '0';
+  key.addEventListener('click', calculate);
+});
 
-    let keyContent = this.textContent;
-    let displayContent = display.textContent;
+let firstValue = '';
+let operator = '';
+let secondValue = '';
+let previouseAction;
 
-    //display clicked numbers
-    if (key.classList.contains('operand')) {
-      previousAction = 'number';
-      if (displayContent == '0' || isOperatorClicked) {
-        display.textContent = keyContent;
-        isOperatorClicked = false;
-      } else {
-        display.textContent += keyContent;
-      }
+function calculate() {
+  let keyContent = this.textContent;
+  let keyClass = this.classList;
+  let CurrentDisplayValueContent = CurrentDisplayValue.textContent;
+  secondValue = CurrentDisplayValueContent;
+
+  removeOperatorStyle();
+
+  if (keyClass.contains('operand')) {
+    if (CurrentDisplayValueContent == '0') {
+      CurrentDisplayValue.textContent = keyContent;
+    } else {
+      CurrentDisplayValue.textContent += keyContent;
     }
+    previouseAction = 'operend';
+  }
 
-    //add decimal
-    if (key.classList.contains('decimal')) {
-      if (!value.includes('.')) {
-        display.textContent = displayContent + '.';
-      }
-    }
+  if (
+    keyClass.contains('decimal') &&
+    !CurrentDisplayValueContent.includes('.')
+  ) {
+    CurrentDisplayValue.textContent = CurrentDisplayValueContent + keyContent;
+  }
+  
+  if (keyClass.contains('operator') && CurrentDisplayValueContent !== '0') {
+    keyClass.add('active'); //add outline styles to operator
 
-    //when an operator is clicked
-    if (key.classList.contains('operator')) {
-      this.classList.add('active');
-      isOperatorClicked = true;
-
-      let secondValue = displayContent;
-      let firstValue = display.dataset.firstValue;
-      let operator = display.dataset.operator;
-
-      if (firstValue && operator && previousAction !== 'operator') {
-      
-        display.textContent = operate(
-          Number(firstValue),
-          operator,
-          Number(secondValue)
-        );
-          
-        display.dataset.firstValue = display.textContent;
-      } else {
-        display.dataset.firstValue = displayContent;
-      }
-
-      display.dataset.operator = keyContent;
-      previousAction = 'operator';
-    }
-
-    
-    if (key.classList.contains('del')) {
-      let newStr = displayContent.split('');
-      if (newStr.length > 1) {
-        newStr.pop();
-        display.textContent = newStr.join('');
-      } else {
-        display.textContent = '0';
-      }
-    }
- 
-    //reset
-    if (key.classList.contains('clear')) {
-      display.textContent = '0';
-      display.dataset.firstValue=""
-    }
-
-    //equals to
-    if (key.classList.contains('equal')) {
-      let firstValue = display.dataset.firstValue;
-      let secondValue = display.textContent;
-      let operator = display.dataset.operator;
-
-      console.log(secondValue);
-      display.textContent = operate(
+    if (firstValue !== '' && operator !== '' && previouseAction !== 'equal') {
+      CurrentDisplayValue.textContent = operate(
         Number(firstValue),
         operator,
         Number(secondValue)
       );
 
-      display.dataset.firstValue=""
-      previousAction = 'equal';
+      firstValue = CurrentDisplayValue.textContent;
+
+      previousDisplayValue.textContent =
+        CurrentDisplayValue.textContent + keyContent;
+
+      CurrentDisplayValue.textContent = '';
+    } else {
+      firstValue = CurrentDisplayValueContent;
+      previousDisplayValue.textContent =
+        CurrentDisplayValueContent + keyContent;
+      CurrentDisplayValue.textContent = '';
     }
-  });
-});
+
+    operator = keyContent;
+
+    previouseAction = 'operator';
+  }
+
+  if (keyClass.contains('equal')) {
+    // secondValue = CurrentDisplayValueContent;
+
+    previousDisplayValue.textContent = `${firstValue} ${operator} ${secondValue} ${keyContent}`;
+    CurrentDisplayValue.textContent = operate(
+      Number(firstValue),
+      operator,
+      Number(secondValue)
+    );
+
+    previouseAction = 'equal';
+  }
+
+  resetCalculator(keyClass);
+  del(keyClass);
+}
+
+function resetCalculator(keyClass) {
+  if (keyClass.contains('clear')) {
+    CurrentDisplayValue.textContent = 0;
+    previousDisplayValue.textContent = '';
+    firstValue = '';
+    secondValue = '';
+    operator = '';
+  }
+}
+
+function del(keyClass) {
+  if (keyClass.contains('del')) {
+    let newStr = CurrentDisplayValue.textContent.split('');
+    if (newStr.length > 1) {
+      newStr.pop();
+      CurrentDisplayValue.textContent = newStr.join('');
+    } else {
+      CurrentDisplayValue.textContent = 0;
+    }
+  }
+}
 
 function operate(operandA, operator, operandB) {
   if (operator == '+') {
@@ -106,7 +118,7 @@ function operate(operandA, operator, operandB) {
   if (operator == 'x') {
     return multiply(operandA, operandB);
   }
-  if (operator == '/') {
+  if (operator == '÷') {
     return divide(operandA, operandB);
   }
 }
@@ -122,4 +134,10 @@ function multiply(a, b) {
 }
 function divide(a, b) {
   return a / b;
+}
+
+function removeOperatorStyle() {
+  keys.forEach((key) => {
+    key.classList.remove('active');
+  });
 }
