9458371372c73d8d8801d581a89dc689fadbc662
Jonathan <jonathankendrick697@gmail.com>
2023-08-07 00:37:47 -0700

    Did not like the first implementation
    the first implementation didnt looked clean and it was harder to understand unlike this one.

--- app.js
+++ app.js
@@ -1,149 +1,128 @@
-//when keys are pressed the number should be displayed on the calculator screen
-//save the first and second value  in a variable
-//when and operator is pressed right after a number was pressed the number and the operator should be diplay in the previous-oprend div
-//you should be able to do basic operator with the calculator and display the answers in a div
+//get all the calculator btns
+//display the key clicked with the correct content
+//save the first value
+// save the operator
+// save the second value
+//create a function to called operate that that take the first value operator and the second value and return and display the answer
+//when and operator is clicked after the first value is present and operator is present display answer plus the opertor clicked
 
-let keys = document.querySelectorAll('.key');
-let previousDisplayValue = document.querySelector('.previous-operand');
-let CurrentDisplayValue = document.querySelector('.current-operand');
+//the caculator should be able to do basic math
+
+const previouDisplay = document.querySelector('.previous-operand');
+const currentDisplay = document.querySelector('.current-operand');
+const keys = document.querySelectorAll('.key');
 
 keys.forEach((key) => {
-  CurrentDisplayValue.textContent = '0';
   key.addEventListener('click', calculate);
 });
 
-let firstValue = '';
-let operator = '';
-let secondValue = '';
-let previouseAction;
-let isOperatorClicked;
-
 function calculate() {
-  removeOperatorStyle();
-
   let keyContent = this.textContent;
   let keyClass = this.classList;
-  let CurrentDisplayValueContent = CurrentDisplayValue.textContent;
-  secondValue = CurrentDisplayValueContent;
+  let currentDisplayValue = currentDisplay.textContent;
+
+  let firstValue = currentDisplay.dataset.firstValue;
+  let operator = currentDisplay.dataset.operator;
+  let secondValue = currentDisplayValue;
+  let previousKeyType = currentDisplay.dataset.previousKeyType;
+
+  removeStyleFromOperator();
 
   if (keyClass.contains('operand')) {
-    if (CurrentDisplayValueContent == '0' || isOperatorClicked) {
-      isOperatorClicked = false;
-      CurrentDisplayValue.textContent = keyContent;
+    showDisplayValue();
+    if (currentDisplayValue == 0 || previousKeyType == 'operator') {
+      currentDisplay.textContent = keyContent;
     } else {
-      CurrentDisplayValue.textContent += keyContent;
+      currentDisplay.textContent += keyContent;
     }
-    previouseAction = 'operend';
+    currentDisplay.dataset.previousKeyType = 'number';
   }
 
-  if (
-    keyClass.contains('decimal') &&
-    !CurrentDisplayValueContent.includes('.')
-  ) {
-    CurrentDisplayValue.textContent = CurrentDisplayValueContent + keyContent;
+  if (keyClass.contains('decimal') && !currentDisplayValue.includes('.')) {
+    currentDisplay.textContent = currentDisplayValue + '.';
   }
 
   if (keyClass.contains('operator')) {
-    this.classList.add('active'); //add outline styles to operator
+    this.classList.add('active'); //add style to the clicked operator
 
-    if (
-      firstValue !== '' &&
-      operator !== '' &&
-      previouseAction !== 'operator' &&
-      previouseAction !== 'equal'
-    ) {
-      console.log(firstValue, operator, secondValue);
-      CurrentDisplayValue.textContent = operate(
-        Number(firstValue),
-        operator,
-        Number(secondValue)
-      );
-
-      firstValue = CurrentDisplayValue.textContent;
-      previousDisplayValue.textContent = firstValue + keyContent;
+    if (firstValue && operator && previousKeyType == 'number') {
+      currentDisplay.textContent = operate(firstValue, operator, secondValue);
+      currentDisplay.dataset.firstValue = currentDisplay.textContent;
     } else {
-      firstValue = CurrentDisplayValueContent;
-      previousDisplayValue.textContent = firstValue + keyContent;
+      currentDisplay.dataset.firstValue = currentDisplay.textContent;
     }
-
-    operator = keyContent;
-    isOperatorClicked = true;
-    previouseAction = 'operator';
+    currentDisplay.dataset.operator = keyContent;
+    previouDisplay.textContent = currentDisplay.textContent + keyContent;
+    currentDisplay.dataset.previousKeyType = 'operator';
+    hideDisplayValue();
   }
 
   if (keyClass.contains('equal')) {
-    if (
-      firstValue !== '' &&
-      operator !== '' &&
-      secondValue !== '' &&
-      previouseAction !== 'equal'
-    ) {
-      previousDisplayValue.textContent = `${firstValue} ${operator} ${secondValue} ${keyContent}`;
-      CurrentDisplayValue.textContent = operate(
-        Number(firstValue),
-        operator,
-        Number(secondValue)
-      );
-      previouseAction = 'equal';
+    if (firstValue && operator && previousKeyType !== 'equal') {
+      previouDisplay.textContent = `${firstValue} ${operator} ${secondValue} =`;
+      currentDisplay.textContent = operate(firstValue, operator, secondValue);
+      currentDisplay.dataset.previousKeyType = 'equal';
     }
   }
 
-  resetCalculator(keyClass);
-  del(keyClass);
-}
-
-function resetCalculator(keyClass) {
   if (keyClass.contains('clear')) {
-    CurrentDisplayValue.textContent = 0;
-    previousDisplayValue.textContent = '';
-    firstValue = '';
-    secondValue = '';
-    operator = '';
+    currentDisplay.dataset.firstValue = '';
+    currentDisplay.dataset.operator = '';
+    previouDisplay.textContent = '';
+    currentDisplay.textContent = '0';
+    currentDisplay.dataset.previousKeyType = 'clear';
   }
-}
 
-function del(keyClass) {
   if (keyClass.contains('del')) {
-    let newStr = CurrentDisplayValue.textContent.split('');
+    let newStr = currentDisplayValue.split('');
     if (newStr.length > 1) {
       newStr.pop();
-      CurrentDisplayValue.textContent = newStr.join('');
+      currentDisplay.textContent = newStr.join('');
     } else {
-      CurrentDisplayValue.textContent = 0;
+      currentDisplay.textContent = '0';
     }
   }
 }
 
-function operate(operandA, operator, operandB) {
+function removeStyleFromOperator() {
+  keys.forEach((key) => key.classList.remove('active'));
+}
+
+function showDisplayValue() {
+  currentDisplay.style.opacity = '1';
+}
+
+function hideDisplayValue() {
+  currentDisplay.style.opacity = '0';
+}
+
+function operate(firstValue, operator, secondValue) {
   if (operator == '+') {
-    return add(operandA, operandB);
+    return add(firstValue, secondValue);
   }
   if (operator == '-') {
-    return subtract(operandA, operandB);
-  }
-  if (operator == 'x') {
-    return multiply(operandA, operandB);
+    return subtract(firstValue, secondValue);
   }
   if (operator == '÷') {
-    return operandB == 0 ? 'DUMBASS' : divide(operandA, operandB);
+    return division(firstValue, secondValue);
+  }
+  if (operator == 'x') {
+    return multiply(firstValue, secondValue);
   }
 }
 
 function add(a, b) {
   return Number(a) + Number(b);
 }
+
 function subtract(a, b) {
   return Number(a) - Number(b);
 }
+
 function multiply(a, b) {
   return Number(a) * Number(b);
 }
-function divide(a, b) {
-  return Number(a) / Number(b);
-}
 
-function removeOperatorStyle() {
-  keys.forEach((key) => {
-    key.classList.remove('active');
-  });
+function division(a, b) {
+  return Number(a) / Number(b);
 }
